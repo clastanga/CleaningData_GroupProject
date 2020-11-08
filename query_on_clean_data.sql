3a363af32bf5f6b8dab465a0fda96d13d1ee27d9
Claudia Stangarone <claudia.stangarone@gmail.com>
2020-11-08 17:52:28 +0100

    new notebook with the whole dataset

--- query_on_clean_data.sql
+++ query_on_clean_data.sql
@@ -21,17 +21,19 @@ order by num_reviews desc
 limit 100 
 ;
 
-select distinct *
+
+select distinct count(*)
 from alldata
 where position regexp 'Data Science';
 
-select distinct *
+
+select distinct count(*)
 from alldata
 where position regexp 'Data Engineering';
 
-select distinct *
+select distinct count(*)
 from alldata
-where position regexp 'Data Analytics';
+where position regexp 'Analyst';
 
 select distinct *
 from alldata
@@ -40,3 +42,16 @@ where position regexp 'machine learning';
 select distinct *
 from alldata
 where position regexp 'Deep learning';
+
+create table description (
+position_id int not null,
+description text
+);
+
+select *
+from description;
+
+select *
+from alldata a
+left join description d
+on a.position_id = d.position_id;
